cd24d00ca1494e11f740a6289970a5f39b628704
Alex Wituk <26alexwituk@usd266.com>
2024-01-08 10:16:39 -0600

    finished 1.1 and 1.2

--- Unit01/Topic1_1/Main.java
+++ Unit01/Topic1_1/Main.java
@@ -2,7 +2,10 @@ package Unit01.Topic1_1;
 
 public class Main {
   public static void main(String[] args) {
-    // Your code goes here
+    System.out.println("Hello World");
+    System.out.println("123!@#$%^&*()-");  
+    int marblesInJar = 3;
+    int otherNumber = 2;
+    System.out.println(marblesInJar * otherNumber);
   }
-  
 }
