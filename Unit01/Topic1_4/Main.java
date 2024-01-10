7558670d1742cb552ff91a9d0455363d50717207
Alex Wituk <26alexwituk@usd266.com>
2024-01-10 10:02:14 -0600

    finished 1.4

--- Unit01/Topic1_4/Main.java
+++ Unit01/Topic1_4/Main.java
@@ -2,7 +2,16 @@ package Unit01.Topic1_4;
 
 public class Main {
   public static void main(String[] args) {
-    // Your code goes here
+    int x = 6;
+    x += 7;
+    x -= 3;
+    x *= 10;
+    x /= 5;
+    x %= 3;
+    System.out.println(x);
+
+    int y = 3;
+    System.out.println(y++);
   }
-  
+
 }
--- Unit01/Topic1_4/README.md
+++ Unit01/Topic1_4/README.md
@@ -1,1 +1,11 @@
 # 1.4 Compound Assignment Operators
+
+## Compound
+x += 7; - take x then add 7 then assign result back to x
+
+x += 7; same as x = x + 7;
+
+## Increment and Decrement Operators
+x++; - takes x then adds 1 then assigns result back to x
+
+x++; same as x = x + 1;
