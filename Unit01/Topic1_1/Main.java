545012e2a890d74c90336dfd883a632c58dca06a
Alex Wituk <26alexwituk@usd266.com>
2024-01-10 09:14:00 -0600

    finished 1.1

--- Unit01/Topic1_1/Main.java
+++ Unit01/Topic1_1/Main.java
@@ -4,8 +4,5 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World");
     System.out.println("123!@#$%^&*()-");  
-    int marblesInJar = 3;
-    int otherNumber = 2;
-    System.out.println(marblesInJar * otherNumber);
   }
 }
