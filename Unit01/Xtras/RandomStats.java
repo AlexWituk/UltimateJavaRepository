829d953caba891f40ec9b9491e876432ef12bf05
Alex Wituk <26alexwituk@usd266.com>
2024-01-12 10:15:29 -0600

    finished, made shorter

--- Unit01/Xtras/RandomStats.java
+++ Unit01/Xtras/RandomStats.java
@@ -15,14 +15,9 @@ public class RandomStats {
 
         System.out.println("Average: " + (n1+n2+n3+n4+n5)/5);
 
-        double g1 = Math.min(n1, n2);
-        double g2 = Math.min(n3, n4);
-        double g3 = Math.min(g1, g2);
-        System.out.println("Minimum: " + Math.min(g3,n5));
+        
+        System.out.println("Minimum: " + Math.min(Math.min(Math.min(n1,n2),Math.min(n3,n4)),n5));
 
-        g1 = Math.max(n1, n2);
-        g2 = Math.max(n3, n4);
-        g3 = Math.max(g1, g2);
-        System.out.println("Maximum: " + Math.max(g3,n5));
+        System.out.println("Maximum: " + Math.max(Math.max(Math.max(n1,n2),Math.max(n3,n4)),n5));
     }
 }
