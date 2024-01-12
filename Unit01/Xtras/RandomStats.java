666486a8ea14110d4db7e246934f234e74cf9dfa
Alex Wituk <26alexwituk@usd266.com>
2024-01-12 09:35:26 -0600

    finished exercises

--- Unit01/Xtras/RandomStats.java
+++ Unit01/Xtras/RandomStats.java
@@ -2,6 +2,27 @@ package Unit01.Xtras;
 
 public class RandomStats {
     public static void main(String[] args) {
+        double n1 = Math.random();
+        System.out.println(n1);
+        double n2 = Math.random();
+        System.out.println(n2);
+        double n3 = Math.random();
+        System.out.println(n3);
+        double n4 = Math.random();
+        System.out.println(n4);
+        double n5 = Math.random();
+        System.out.println(n5);
 
+        System.out.println("Average: " + (n1+n2+n3+n4+n5)/5);
+
+        double g1 = Math.min(n1, n2);
+        double g2 = Math.min(n3, n4);
+        double g3 = Math.min(g1, g2);
+        System.out.println("Minimum: " + Math.min(g3,n5));
+
+        g1 = Math.max(n1, n2);
+        g2 = Math.max(n3, n4);
+        g3 = Math.max(g1, g2);
+        System.out.println("Maximum: " + Math.max(g3,n5));
     }
 }
--- Unit01/Xtras/TwoDSix.java
+++ Unit01/Xtras/TwoDSix.java
@@ -2,6 +2,9 @@ package Unit01.Xtras;
 
 public class TwoDSix {
     public static void main(String[] args) {
+        double d1 = Math.random() * 6 + 1;
+        double d2 = Math.random() * 6 + 1;
 
+        System.out.println(((int) d1) + ((int) d2));
     }
 }
