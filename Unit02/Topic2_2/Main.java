23cf420ceefa0c8c82e978cebbf0db306b377df7
Alex Wituk <26alexwituk@usd266.com>
2024-01-18 10:04:56 -0600

    finished 2.2

--- Unit02/Topic2_2/Main.java
+++ Unit02/Topic2_2/Main.java
@@ -1,8 +1,26 @@
 package Unit02.Topic2_2;
 
-public class Main {
-  public static void main(String[] args) {
 
+public class Main {
+  public class Employee {
+    private String name;
+    private String empId;
+    private int pay;
+
+    public Employee() {
+      name = null;
+      empId = null;
+      pay = 0;
+    }
+
+    public Employee(String nm, String id, int py) {
+      name = nm;
+      empId = id;
+      pay = py;
+    }
   }
 
+  Employee person1 = new Employee("Simon", "7888", 60000);
+  public static void main(String[] args) {
+  }
 }
--- Unit02/Topic2_2/README.md
+++ Unit02/Topic2_2/README.md
@@ -1,1 +1,14 @@
 # 2.2 Creating and Storing Objects (Instantiation)
+Constructors are used to initialize the attributes for an object
+
+`public Person(String nm, int ag, boolean ad) {}`
+
+Formal parameters in parathesis ^^
+
+`Person alexPerson = new Person("Alex", 15, false)`
+
+Actual parameters in parathesis ^^
+
+No-Argument/Default constructuor has no parameters
+
+Constructors are the different blueprints you make for a new variable to fit into
