8b9806ffb86c40e09a3a01b215717ef53747a031
Alex Wituk <26alexwituk@usd266.com>
2024-01-19 10:13:10 -0600

    finished 2.4

--- Unit02/Topic2_4/Main.java
+++ Unit02/Topic2_4/Main.java
@@ -1,7 +1,15 @@
 package Unit02.Topic2_4;
 
 public class Main {
+  public static void calcAverage(int num1, int num2) {
+    int sum = num1+num2;
+    System.out.println(sum);
+  }
+
+
   public static void main(String[] args) {
+    
+    Main.calcAverage(2,3);
 
   }
 
--- Unit02/Topic2_4/README.md
+++ Unit02/Topic2_4/README.md
@@ -1,1 +1,4 @@
 # 2.4 Calling a Void Method with Parameters
+Example of Method With Parameters
+
+`public static void calcAverage(int num1, int num2) {}`
