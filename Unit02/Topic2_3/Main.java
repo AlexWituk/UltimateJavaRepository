5fa4add8de9e8a97e5f49f5598fe03c7cc43ab7f
Alex Wituk <26alexwituk@usd266.com>
2024-01-19 09:42:32 -0600

    finished 2.3

--- Unit02/Topic2_3/Main.java
+++ Unit02/Topic2_3/Main.java
@@ -1,8 +1,26 @@
 package Unit02.Topic2_3;
 
 public class Main {
-  public static void main(String[] args) {
+  public class Lamp {
+    
+    private boolean isOn;
+
+    public void turnOn() {
+      isOn = true;
+      System.out.println("Lamp on");
+    }
+
+    public void turnOff() {
+      isOn = false;
+      System.out.println("Lamp off");
+    }
+  
+  
+
+
 
   }
-
+  public static void main(String[] args) {
+  }
 }
+
--- Unit02/Topic2_3/README.md
+++ Unit02/Topic2_3/README.md
@@ -1,1 +1,3 @@
 # 2.3 Calling a Void Method
+Methods - define the behaviors for all objects of a class and consist of a set of instructions for excuting a behaviour
+
