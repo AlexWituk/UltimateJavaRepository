693d8104d39319c17dd1be32bd875d82e20033db
Alex Wituk <26alexwituk@usd266.com>
2024-01-22 10:00:28 -0600

    finished 2.5

--- Unit02/Topic2_5/Main.java
+++ Unit02/Topic2_5/Main.java
@@ -1,6 +1,14 @@
 package Unit02.Topic2_5;
 
 public class Main {
+  
+  
+  public double divideBill(int numFriends, double totalBill) {
+    double amtPerFriend = totalBill / numFriends;
+    return amtPerFriend;
+  }
+  
+
   public static void main(String[] args) {
 
   }
--- Unit02/Topic2_5/README.md
+++ Unit02/Topic2_5/README.md
@@ -1,1 +1,6 @@
 # 2.5 Calling a Non-void Method
+Non-void methods return a value that is the same as the signature
+
+`public double thingy() {
+    return double;
+}`
