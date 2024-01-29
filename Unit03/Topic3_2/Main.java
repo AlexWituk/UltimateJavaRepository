3ede73faf38820a6c003facca098d5462524e818
Alex Wituk <26alexwituk@usd266.com>
2024-01-29 10:16:41 -0600

    finished 3.2

--- Unit03/Topic3_2/Main.java
+++ Unit03/Topic3_2/Main.java
@@ -2,6 +2,11 @@ package Unit03.Topic3_2;
 
 public class Main {
   public static void main(String[] args) {
+    int myAge = 15;
+    if (myAge == 15) {
+      System.out.println("you are 15");
+    }
+
 
   }
 
