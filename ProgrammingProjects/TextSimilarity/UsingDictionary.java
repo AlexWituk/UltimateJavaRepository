7f815e8305747825961b46ee38c2d5cbd6e72ce8
Alex Wituk <26alexwituk@usd266.com>
2024-03-29 10:36:25 -0500

    fin 8.1

--- ProgrammingProjects/TextSimilarity/UsingDictionary.java
+++ ProgrammingProjects/TextSimilarity/UsingDictionary.java
@@ -4,6 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
+import org.apache.commons.text.similarity.FuzzyScore;
+import org.apache.commons.text.similarity.LevenshteinDistance;
 
 public class UsingDictionary {
 
@@ -23,6 +28,39 @@ public class UsingDictionary {
     }
 
     public static void main(String[] args) {
-        // Your code here
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Please enter your word: ");
+        String word = s.nextLine();
+        s.close();
+
+        FuzzyScore fuzzyNum = new FuzzyScore(Locale.getDefault());
+        LevenshteinDistance lDistance = new LevenshteinDistance();
+
+
+        int highestFuzzy = 0;
+        String bestFuzzy = "";
+        for (String e : getEnglishWords()) {
+            if (fuzzyNum.fuzzyScore(word, e) > highestFuzzy) {
+                highestFuzzy = fuzzyNum.fuzzyScore(word, e);
+                bestFuzzy = e;
+            }
+        }
+
+        System.out.println("Closest word using fuzzy algorithm was: " + bestFuzzy);
+
+        int lowestLev = Integer.MAX_VALUE;
+        String bestLev = "";
+        for (String e : getEnglishWords()) {
+            if (lDistance.apply(word, e) < lowestLev) {
+                lowestLev = lDistance.apply(word, e);
+                bestLev = e;
+            }
+            
+        }
+
+        System.out.println("Closest word using Levenshtein algoritm was: " + bestLev);
+
+
     }
 }
--- Unit08/README.md
+++ Unit08/README.md
@@ -1,5 +1,14 @@
 # Unit 8 - 2D Array
 
 ## 8.1 - 2D Array
+2D Array - An array made out of arrays      
+
+Initilization
+> new DataType[ number of rows ] [columns/length of each array row]     
+new int [7] [4]
+
+You can access values inside a 2D array by using:
+> arrayName[row][column]
+
 
 ## 8.2 - Transversing 2D Arrays
